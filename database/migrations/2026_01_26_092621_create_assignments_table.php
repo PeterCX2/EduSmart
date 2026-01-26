6f3aafc40ba77bc5ad5b3130cb3d723ab907f93b
PeterCleon <petercleon.1234@gmail.com>
2026-01-26 16:34:04 +0700

    feat: database and tables

---
+++ database/migrations/2026_01_26_092621_create_assignments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->unsignedBigInteger('subject_id')->nullable();
+            $table->date('deadline')->nullable();
+            $table->string('created_by')->nullable();
+            $table->timestamps();
+            $table->foreign('subject_id')->references('id')->on('subjects')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assignments');
+    }
+};
