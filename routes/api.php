2d85b5237fd43e1f7c72d5275d915b76171b4ccb
PeterCleon <petercleon.1234@gmail.com>
2026-01-27 11:00:18 +0700

    feat: Dashboard

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AssignmentController;
+use App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +8,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/subject/', [AssignmentController::class, 'index'])->name('index');
-Route::post('/subject/assignment', [AssignmentController::class, 'store'])->name('store');
+Route::get('/Dashboard', [Controllers\DashboardController::class, 'index'])->name('index');
+
+Route::get('/subject/', [Controllers\AssignmentController::class, 'index'])->name('index');
+Route::post('/subject/assignment', [Controllers\AssignmentController::class, 'store'])->name('store');
