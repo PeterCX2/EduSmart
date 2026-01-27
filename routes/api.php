be8d18c6a520ee8b26b16ae03f60d4eb353cbe9b
CloudiosV <cloudiosss@gmail.com>
2026-01-27 12:25:53 +0700

    feat: school CRUD and routes

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get('/', [Controllers\SchoolController::class, 'index'])->name('index');
+Route::post('/school', [Controllers\SchoolController::class, 'store'])->name('store');
+Route::put('/school/{school}', [Controllers\SchoolController::class, 'update'])->name('update');
+Route::delete('/school/{school}', [Controllers\SchoolController::class, 'destroy'])->name('destroy');
+
 Route::get('/Dashboard', [Controllers\DashboardController::class, 'index'])->name('index');
 
 Route::get('/subject/', [Controllers\AssignmentController::class, 'index'])->name('index');
