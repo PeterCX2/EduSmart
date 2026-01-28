2905ef0a4c78c9a010e8f8af9f559ed8eff3b6fd
CloudiosV <cloudiosss@gmail.com>
2026-01-28 14:29:36 +0700

    feat: adding permissions, roles, and super admin account

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -14,9 +14,57 @@ class RolePermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        $superAdmin = Role::create(['name' => 'super-admin']);
-        $teacher = Role::create(['name' => 'teacher']);
-        $student = Role::create(['name' => 'student']);
-        $user = Role::create(['name' => 'user']);
+        $permissions = [
+            ['name' => 'view schools'],
+            ['name' => 'create schools'],
+            ['name' => 'edit schools'],
+            ['name' => 'delete schools'],
+
+            ['name' => 'view users'],
+            ['name' => 'create users'],
+            ['name' => 'edit users'],
+            ['name' => 'delete users'],
+            
+            ['name' => 'view subjects'],
+            ['name' => 'create subjects'],
+            ['name' => 'edit subjects'],
+            ['name' => 'delete subjects'],
+            
+            ['name' => 'view assignments'],
+            ['name' => 'create assignments'],
+            ['name' => 'edit assignments'],
+            ['name' => 'delete assignments'],
+
+            ['name' => 'view submissions'],
+            ['name' => 'create submissions'],
+            ['name' => 'grade submissions'],
+            ['name' => 'delete submissions'],
+
+            ['name' => 'view submission_feedback'],
+            ['name' => 'create submission_feedback'],
+            ['name' => 'delete submission_feedback'],
+
+            ['name' => 'view roles'],
+            ['name' => 'create roles'],
+            ['name' => 'edit roles'],
+            ['name' => 'delete roles'],
+        ];
+        
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission['name'],
+                'guard_name' => 'web'
+            ]);
+        }
+
+        $superAdminRole = Role::firstOrCreate(['name' => 'super-admin', 'guard_name' => 'web']);
+        
+        $superAdminRole->syncPermissions(Permission::all());
+
+        \App\Models\User::firstOrCreate([
+            'name' => 'SuperAdmin',
+            'email' => 'superadmin@gmail.com',
+            'password' => \Illuminate\Support\Facades\Hash::make('superadmin123'),
+        ])->assignRole('super-admin');
     }
 }
